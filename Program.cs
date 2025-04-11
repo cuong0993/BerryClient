29711d69fe2ff2b9359cfd1b7d0688e254496f0a
Cương Nguyễn <cuong0993@gmail.com>
2025-04-11 14:28:05 +0700

    Add

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using ContentGraphClientNS;
+using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+services.AddHttpClient("ContentGraphClient",
+    client =>
+    {
+        client.BaseAddress =
+            new Uri("https://cg.optimizely.com/content/v2?auth=eBrGunULiC5TziTCtiOLEmov2LijBf30obh0KmhcBlyTktGZ");
+    });
+services.AddContentGraphClient();
+var serviceProvider = services.BuildServiceProvider();
+
+var contentGraphClient = serviceProvider.GetRequiredService<IContentGraphClient>();
+var data = contentGraphClient.GetProductPages.ExecuteAsync().Result;
---
+++ README.md
@@ -0,0 +1,4 @@
+```shell
+dotnet graphql init https://cg.optimizely.com/content/v2?auth=eBrGunULiC5TziTCtiOLEmov2LijBf30obh0KmhcBlyTktGZ -n ContentGraphClient
+dotnet graphql generate
+```
